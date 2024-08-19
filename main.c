f12553a6f6cc1fea59f20a4963b562e839fc5f43
shima8823 <taiyaki1275@gmail.com>
2024-08-19 07:13:33 +0000

    add tests, str(len | cpy | cmp)

--- main.c
+++ main.c
@@ -1,12 +1,30 @@
 #include "main.h"
 
-// void test_strlen() {
-// 	char *buf = "42tokyo\n";
-// 	printf("%zu", strlen(buf));
-// 	printf("%zu", ft_strlen(buf));
-// }
+void test_strlen() {
+	char *buf = "42tokyo\n";
+	printf("%zu\n", strlen(buf));
+	printf("%zu\n", ft_strlen(buf));
+}
+
+void test_strcpy() {
+	char dest[10];
+	char *src = "42tokyo\n";
+	printf("%s", strcpy(dest, src));
+	printf("%s", ft_strcpy(dest, src));
+}
+
+void test_strcmp() {
+	char *s1 = "42tokyo\n";
+	char *s2 = "42paris\n";
+	printf("%d\n", strcmp(s1, s2));
+	printf("%d\n", ft_strcmp(s1, s2));
+
+	printf("%d\n", strcmp(s1, s1));
+	printf("%d\n", ft_strcmp(s1, s1));
+}
 
 int main() {
-	// test_strlen();
-	hello();
+	test_strlen();
+	test_strcpy();
+	test_strcmp();
 }
--- main.h
+++ main.h
@@ -4,7 +4,10 @@
 #include <stdio.h>
 #include <string.h>
 
-// extern size_t ft_strlen(const char *s);
-extern void hello();
+
+void	hello();
+size_t	ft_strlen(const char *s);
+char	*ft_strcpy(char * dst, const char * src);
+int		ft_strcmp(const char *s1, const char *s2);
 
 #endif
