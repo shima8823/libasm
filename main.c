1ec84c2e48923f9da19a9190da793bc14014c426
shima8823 <taiyaki1275@gmail.com>
2024-08-20 07:42:21 +0000

    add read test

--- main.c
+++ main.c
@@ -43,9 +43,67 @@ void test_write() {
 	
 }
 
+void compare_reads(const char *test_name, int fd, size_t count) {
+	char buf_std[BUFFER_SIZE] = {0};
+	char buf_ft[BUFFER_SIZE] = {0};
+	ssize_t read_std, read_ft;
+	int errno_std, errno_ft;
+
+	// std
+	errno = 0;
+	read_std = read(fd, buf_std, count);
+	errno_std = errno;
+
+	if (fd != STDIN_FILENO) {
+		lseek(fd, 0, SEEK_SET);
+	}
+
+	// ft
+	errno = 0;
+	read_ft = ft_read(fd, buf_ft, count);
+	errno_ft = errno;
+
+	// test
+	printf("Test: %s\n", test_name);
+	printf("Standard read: bytes=%zd, errno=%d, content='%.20s%s'\n", 
+		   read_std, errno_std, buf_std, (strlen(buf_std) > 20 ? "..." : ""));
+	printf("ft_read:       bytes=%zd, errno=%d, content='%.20s%s'\n", 
+		   read_ft, errno_ft, buf_ft, (strlen(buf_ft) > 20 ? "..." : ""));
+		
+	if (read_std == read_ft && errno_std == errno_ft && (read_std <= 0 || memcmp(buf_std, buf_ft, read_std) == 0)) {
+		printf("Result: PASS\n");
+	} else {
+		printf("Result: FAIL\n\n");
+	}
+}
+
+void test_file_read(const char *filename) {
+	int fd = open(filename, O_RDONLY);
+	if (fd == -1) {
+		perror("open failed");
+		return;
+	}
+
+	compare_reads("File read", fd, BUFFER_SIZE);
+	close(fd);
+}
+
+void test_error_cases() {
+	compare_reads("Invalid file descriptor", -1, BUFFER_SIZE);
+	compare_reads("NULL buffer", STDIN_FILENO, 0);
+}
+
+
+void test_read() {
+	printf("Comparing standard read with ft_read:\n\n");
+	test_file_read("test.txt");
+	test_error_cases();	
+}
+
 int main() {
 	test_strlen();
 	test_strcpy();
 	test_strcmp();
 	test_write();
+	test_read();
 }
--- main.h
+++ main.h
@@ -5,11 +5,15 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 1024
 
 void	hello();
 size_t	ft_strlen(const char *s);
 char	*ft_strcpy(char * dst, const char * src);
 int		ft_strcmp(const char *s1, const char *s2);
 ssize_t	ft_write(int fd, const void *buf, size_t count);
+ssize_t	ft_read(int fd, void *buf, size_t count);
 
 #endif
